5a9f7bfd0f6e0de5453ed774cc028b7eba154202
Jacob Hornung <hornunjb@mail.uc.edu>
2021-02-28 20:01:04 -0500

    country service

--- app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
+++ app/src/main/java/app/plantdiary/individualassignment3048q/ui/main/MainViewModel.kt
@@ -1,7 +1,13 @@
 package app.plantdiary.individualassignment3048q.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var countries: MutableLiveData<ArrayList<Plant>> = MutableLiveData()
+    var countryService: CountryService = CountryService()
+
+    fun fetchCountries(countryName: String) {
+        countries = countryService.fetchCountries(countryName)
+    }
 }
